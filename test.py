482db6f65764302a152aafa719371856ff7574db
shanky49-98 <shashankdixit98@gmail.com>
2021-05-30 11:18:21 +0200

    Update test.py
    This change was done in explorer mode.

--- test.py
+++ test.py
@@ -31,7 +31,7 @@ X = X.astype('float32')
 X /= 255
 
 # Adding this comment to test changing in GitHub Desktop
-
+# Changes done in Notepad
 train_datagen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2,zoom_range = 0.2, horizontal_flip = True)
 
 # Generates batches of Augmented Image data
